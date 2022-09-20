f82aad8030b4f179e6a5d3a15ff5cffc343ea7ea
Clayton Schneider <schneiderc2016@gmail.com>
2022-09-20 13:46:16 -0400

    tailwind init

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const { default: plugin } = require("tailwindcss");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        yeseva: ["YesevaOne", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
