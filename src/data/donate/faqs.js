ff0ccee2bdc77369a36b9d3d9bc66a7fcb003a66
Clayton Schneider <schneiderc2016@gmail.com>
2022-09-22 15:01:48 -0400

    donate page first pass

---
+++ src/data/donate/faqs.js
@@ -0,0 +1,12 @@
+export const faq = [
+  {
+    question: "How do I edit the pdf?",
+    answer:
+      "There are text areas where you can type out your responses. Simply open the file in your browser and click into the text areas to type.",
+  },
+  {
+    question: "How should I submit my material?",
+    answer:
+      "You have two options. You can either physically mail all necessary documents to our address. Or, you can scan your supplemental material and email it along with the completed applicaiton.",
+  },
+];
