9a22d74625829f9042ab6aa9ddc3ff7cadbd189a
Clayton Schneider <schneiderc2016@gmail.com>
2022-09-23 10:00:52 -0400

    responsive scholarship page, need to add images

---
+++ src/data/scholarships/scholarships.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    title: "Sydney Alice Alworth",
+    img: "../data/imgs/sydney-allice-alworth.jpg",
+    quote: `keep me as the apple of your eye; \nhide me in the shadow of your wings Psalm 17:8`,
+    body: "This scholarship was established in 2020 by Sydney’s family and friends to encourage a high school graduate to pursue post-secondary education at the institution of his or her choice, including any two- or four-year college or university, technical school, or institution offering post-graduate study. This scholarship was created to honor the life of Sydney Alice Alworth and can be awarded to any student who meets the qualifications of the Roxbury Scholarship Foundation. Sydney was a gifted artist, accomplished musician, and worldwide traveler. She loved this planet and especially loved animals. She was passionate about social and environmental issues, and ally and advocate for our LBGTQ brothers and sisters and a friend to those struggling. Sydney was truly a beautiful person inside and out. She was kind and caring, sweet and sensitive, with a generous spirit and a quirky, witty sense of humor. We are proud and grateful that this scholarship will keep Sydney’s memory alive.",
+  },
+];
