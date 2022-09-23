88d2d4ed109dc3456db9254d04c933b84c337b82
Clayton Schneider <schneiderc2016@gmail.com>
2022-09-23 14:57:38 -0400

    directors responsive

---
+++ src/data/about/directors.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    name: "Joan Temple",
+    title: "Secretary",
+    img: "srcs",
+  },
+  {
+    name: "Ken Allen",
+    title: "Vice President",
+    img: "srcs",
+  },
+  {
+    name: "Lucia Alworth",
+    title: "Director",
+    img: "srcs",
+  },
+  {
+    name: "Kimberly Pokrywka",
+    title: "Director",
+    img: "srcs",
+  },
+  {
+    name: "Nina Shimer",
+    title: "Director",
+    img: "srcs",
+  },
+  {
+    name: "Jan Steinman",
+    title: "Director",
+    img: "srcs",
+  },
+  {
+    name: "Anthony Benivegna",
+    title: "President/Treasurer",
+    img: "srcs",
+  },
+];
