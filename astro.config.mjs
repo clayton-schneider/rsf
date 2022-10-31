552348fe831add62bf2ad914104b09d3dde93d43
Clayton Schneider <schneiderc2016@gmail.com>
2022-10-31 09:30:11 -0400

    stories to ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-
 import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image({ serviceEntryPoint: "@astrojs/image/sharp" }),
-    react(),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), react(), mdx()]
 });
---
+++ src/data/success-stories/successStories.ts
@@ -0,0 +1,119 @@
+export default [
+  {
+    name: "Catrina Cartagena",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/catrina-cartagena.jpg"),
+    year: "2003",
+    body: "Catrina Cartagena studied government and education at Bowdoin College and earned a law degree at Cornell University. As a practicing attorney, Catrina has represented a wide variety of municipalities and non-profits.",
+    slug: "catrina-cartagena",
+  },
+  {
+    name: "Cesira Farrel",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/cesira-farrel.jpg"),
+    year: "2006",
+    body: "Cesira Farrell studied art and design at Savanah College and went on to earn a Ph.D. at Teachers College, Columbia for Applied Behavior Analysis.",
+    slug: "cesira-farrell",
+  },
+  {
+    name: "Chelsea Ale",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/chelsea-ale.jpg"),
+    year: "2000",
+    body: "Dr. Chelsea Ale graduated from Shepaug in 2000 and went on to earn a degree in Psychology at Mt Holyoke College and from there her MS and Ph.D. in Clinical Psychology from West Virginia University. Currently, Dr. Ale holds a position at the Mayo clinic. She is the author of more than 20 peer-reviewed scientific articles and book chapters. She serves on the editorial board of Child Psychiatry & Human Development and Children’s Health Care.",
+    slug: "chelsea-ale",
+  },
+  {
+    name: "Emily Alworth",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/emily-alworth.jpg"),
+    year: "2015",
+    body: `Emily Alworth has the career of her dreams: She is a horticulturist at Red Butte Garden, at the University of Utah. She credits her professors at Sarah Lawrence College for her solid science education, giving her the tools she needed to grow and maintain the newly created zero-scape garden at the University.
+
+    Emily Alworth has the career of her dreams: She is a horticulturist at Red Butte Garden, at the University of Utah. She credits her professors at Sarah Lawrence College for her solid science education, giving her the tools she needs to grow and maintain the newly created zero-scape garden at the University. She enjoys everything about her work, from designing and installing her new garden, giving classes to students interested in all things botanical, and wrastling the occasional wayward rattlesnake.
+
+    “The Roxbury Scholarship Fund made it possible for me to receive a fantastic education. Their support was incredible!"`,
+    slug: "emily-alworth",
+  },
+  {
+    name: "Jamie Reilly",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/jamie-reilly.jpg"),
+    year: "2009",
+    body: `Jamie Reilly works as a research scientist at the United States Military Academy in West Point, NY. After graduating from Boston University with an M.Ed. in Coaching and Sport Psychology in 2015, he accepted a volunteer position at USMA coaching their swim and dive team. Once at USMA, Jamie developed an interest in Chemistry and the Life Sciences and was able to study under the guidance of LTC Luis Alvarez, Ph.D. Since beginning with the USMA CLS Dept. in 2015, Jamie’s work has expanded to include methods for diagnosing osteoarthritis, mTBI/concussions, Lyme disease, and biological engineering relevant to 3D/4D printing of ready-to-implant human organs.
+
+    "Many thanks to the generous people of Roxbury for providing support and launching me into an exciting career of scientific research, biomedical engineering, medical diagnostics, and studying the intersection of technology, medicine and human performance."`,
+    slug: "jamie-reilly",
+  },
+  {
+    name: "Jon Olivieri",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/jon-olivieri.jpg"),
+    year: "2002",
+    body: "Jon Olivieri is an artist and instructor. He graduated from Shepaug in 2002 and while there and afterward, he did extensive field studies in Iceland, New Zealand, and the European Alps. Influenced by these field studies he constructed a striking visual image of the Earth’s structures and patterns. His work is in collections both in Europe and the United States.",
+    slug: "jon-olivieri",
+  },
+  {
+    name: "Kirby Peters",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/kirby-peters.jpg"),
+    year: "2013",
+    body: "Kirby Peters graduated from Shepaug in 2013. He went on to St. Michael’s College to study environmental science and then graduated from Cornell University Agricultural Science program. While at Cornell Kirby was awarded the NYSABA Seed Committee outstanding undergraduate award. Up until recently, Kirby worked as the greenhouse manager at a large diversified vegetable farm in Austin, Texas. He has returned to Roxbury and will be the new manager of Maple Bank Farm.",
+    slug: "kirby-peters",
+  },
+  {
+    name: "Leigh Childs",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/leigh-childs.jpg"),
+    year: "2006",
+    body: `Leigh Childs graduated from Northeastern University in 2011, and inspired by her love of travel, Leigh accepted a position teaching English at a Korean private school in Seoul, South Korea. There she discovered that she loved teaching young children. This motivated her to return to Connecticut to earn her Master's degree in Elementary Education. Leigh now enjoys a career working abroad within the International School system.
+
+    Leigh Childs is currently teaching second grade at the Beijing International Bilingual Academy (BIBA) in China. After graduating from Northeastern University in 2011, and inspired by her love of travel, Leigh accepted a position teaching English at a Korean private school in Seoul, South Korea. There she discovered that she loved teaching young children. This motivated her to return to Connecticut to earn her Masters degree in Elementary Education. As a result, Leigh now enjoys a career working abroad within the International School system.
+
+    "I have to thank the Roxbury Scholarship Foundation for their support. I believe the best way to encourage tolerance and understanding of each other globally is through education. As I witness the individual talents of these amazing children, I hope I am fostering a thirst for knowledge, independent thinking, and curiosity. We can all strive to continuously learn from each other. Thank you, thank you for helping me achieve my dream of teaching abroad."`,
+    slug: "leigh-childs.",
+  },
+  {
+    name: "Marsha Doran",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/marsha-doran.jpg"),
+    year: "1992",
+    body: "Marsha Doran earned a BA in Painting and Illustration at West Conn in 1996 and went on to earn her MFA at Boston University. Marsha is currently the Director of Visual Arts at the Canterbury School in New Milford.",
+    slug: "marsha-doran",
+  },
+  {
+    name: "Rory Larson",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/rory-larson.jpg"),
+    year: "2005",
+    body: `Rory Larson grew up exploring the Shepaug River Valley. Here, he gained an appreciation for nature and decided to pursue a career protecting wild places and their inhabitants. Rory got his degree in wildlife ecology in a top-tier program at the University of Maine and is now the Conservation Science Manager at Steep Rock Association.
+
+    Rory Larson grew up exploring the Shepaug River Valley. Here, he gained an appreciation for nature and decided to pursue a career protecting wild places and their inhabitants. Since 2007, Rory’s work has spanned taxonomic groups and conservation efforts across the country, from sea turtles in Florida to Canada lynx in Maine, spotted owls in the High Sierras of California, and endemic fish in the deserts of Utah. Most recently, he has studied and managed habitat for imperiled species in the Northeast. Rory is now the Conservation Science Manager at Steep Rock Association where he applies all he has learned to enhance the biological diversity and ecological integrity of lands and waters encompassing 5,500 acres in the Washington area.
+
+    “The Roxbury Scholarship Foundation made it possible for me to study wildlife ecology in a top-tier program at the University of Maine, building a critical base of knowledge and experience that prepared me for real world scientific research.”`,
+    slug: "rory-larson",
+  },
+  {
+    name: "Steven Kershnar, Sari Reikes, Eric Kershnar",
+    summary:
+      "Description area for a quick summary about this recipient, what they were awarded and what they are doing",
+    img: import("@imgs/stories/steven-kershnar.jpg"),
+    year: "",
+    body: `Steven Kershnar earned a BA from Cornell, a JD from the University of Pennsylvania and a Ph.D. from the University of Nebraska. He is a philosophy professor and published author.
+
+    Sari Reikes received her BS from UCONN and a Masters in Public Administration from Suffolk University and is the controller at a scientific company in Syracuse NY.
+
+    Eric Kershnar received his BA from Brown University and his MS and Ph.D. from the University of Illinois. He is a research chemist working in New Haven.`,
+    slug: "steven-kershnar",
+  },
+];
