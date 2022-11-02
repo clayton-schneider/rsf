4c6dce41319489421bdbed840efca53d8152a3fd
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-02 12:00:40 -0400

    email form function first attempt

---
+++ functions/_middelware.ts
@@ -0,0 +1,18 @@
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+
+export const onRequest: PagesFunction = mailChannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
+    },
+  ],
+  from: {
+    name: "Website Form Service",
+    email: "noreplay@simply-sprout.com",
+  },
+  respondWith: () => {
+    return new Response(
+      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+    );
+  },
+});
