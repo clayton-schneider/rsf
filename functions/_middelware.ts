6640211b528941532c0d2fc2717ab5557799a9e4
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-02 14:07:09 -0400

    testing

--- functions/_middelware.ts
+++ functions/_middelware.ts
@@ -3,16 +3,12 @@ import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 export const onRequest: PagesFunction = mailChannelsPlugin({
   personalizations: [
     {
-      to: [
-        { name: "Clayton Schneider", email: "clayton@simply-sprout.com" },
-        { name: "behjue", email: "behjue@gmail.com" },
-        { name: "Clayton Schneider", email: "schneiderc2016@gmail.com" },
-      ],
+      to: [{ name: "behjue", email: "behjue@gmail.com" }],
     },
   ],
   from: {
-    name: "Website Form Service",
-    email: "noreplay@simply-sprout.com",
+    name: "Test Sender",
+    email: "sender@example.com",
   },
   respondWith: () => {
     return new Response(
