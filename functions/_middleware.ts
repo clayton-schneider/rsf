3cb204724b2872ef002f95a2811681f16fbb1393
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-04 14:35:42 -0400

    does static forms plugin work?

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,18 +1,28 @@
-import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = mailChannelsPlugin({
-  personalizations: [
-    {
-      to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
-    },
-  ],
-  from: {
-    name: "Website Form Submission",
-    email: "no-reply@simply-sprout.com",
-  },
-  respondWith: () => {
+// export const onRequest: PagesFunction = mailChannelsPlugin({
+//   personalizations: [
+//     {
+//       to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
+//     },
+//   ],
+//   from: {
+//     name: "Website Form Submission",
+//     email: "no-reply@simply-sprout.com",
+//   },
+//   respondWith: () => {
+//     return new Response(
+//       `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+//     );
+//   },
+// });
+import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
+
+export const onRequest: PagesFunction = staticFormsPlugin({
+  respondWith: ({ formData, name }) => {
+    const email = formData.get("email");
     return new Response(
-      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
+      `Hello, ${email}! Thank you for submitting the ${name} form.`
     );
   },
 });
