1bafdd3245c890f652f20617ef5e67d7579f1145
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-04 09:48:25 -0400

    copy paste example

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -1,39 +1,56 @@
-export async function onRequestPost(context) {
-  try {
-    let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
-    let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify({
-        personalizations: [
-          {
-            to: [
-              { email: "clayton@simply-sprout.com", name: "Clayton Schneider" },
-            ],
-          },
-        ],
-        from: {
-          email: "noreply@simply-sprout.com",
-          name: "Workers - MailChannels integration",
-        },
-        subject: "Look! No Servers",
-        content: [
-          {
-            type: "text/plain",
-            value: pretty,
-          },
-        ],
-      }),
-    });
-    return new Response(pretty, {
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-    });
-  } catch (err) {
-    return new Response("Error parsing JSON content");
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  let content = "";
+  for (var i of request.headers.entries()) {
+    content += i[0] + ": " + i[1] + "\n";
   }
+  let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      personalizations: [
+        {
+          to: [
+            { email: "clayton@simply-sprout.com", name: "Clayton Schneider" },
+          ],
+        },
+      ],
+      from: {
+        email: "sender@example.com",
+        name: "Test Sender",
+      },
+      subject: "Test Subject",
+      content: [
+        {
+          type: "text/plain",
+          value: "Test message content\n\n" + content,
+        },
+      ],
+    }),
+  });
+
+  let respContent = "";
+  // only send the mail on "POST", to avoid spiders, etc.
+  if (request.method == "POST") {
+    const resp = await fetch(send_request);
+    const respText = await resp.text();
+
+    respContent = resp.status + " " + resp.statusText + "\n\n" + respText;
+  }
+
+  let htmlContent =
+    "<html><head></head><body><pre>" +
+    `</pre><p>Click to send message: <form method="post"><input type="submit" value="Send"/></form></p>` +
+    "<pre>" +
+    respContent +
+    "</pre>" +
+    "</body></html>";
+  return new Response(htmlContent, {
+    headers: { "content-type": "text/html" },
+  });
 }
