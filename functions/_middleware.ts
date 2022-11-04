ea7fafe1facceb941269279a4ab827441f21ad8e
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-04 14:47:09 -0400

    back to example

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,28 +1,18 @@
-// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-// export const onRequest: PagesFunction = mailChannelsPlugin({
-//   personalizations: [
-//     {
-//       to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
-//     },
-//   ],
-//   from: {
-//     name: "Website Form Submission",
-//     email: "no-reply@simply-sprout.com",
-//   },
-//   respondWith: () => {
-//     return new Response(
-//       `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
-//     );
-//   },
-// });
-import staticFormsPlugin from "@cloudflare/pages-plugin-static-forms";
-
-export const onRequest: PagesFunction = staticFormsPlugin({
-  respondWith: ({ formData, name }) => {
-    const email = formData.get("email");
+export const onRequest: PagesFunction = mailChannelsPlugin({
+  personalizations: [
+    {
+      to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
+    },
+  ],
+  from: {
+    name: "Website Form Submission",
+    email: "no-reply@simply-sprout.com",
+  },
+  respondWith: () => {
     return new Response(
-      `Hello, ${email}! Thank you for submitting the ${name} form.`
+      `Thank you for submitting your enquiry. A member of the team will be in touch shortly.`
     );
   },
 });
