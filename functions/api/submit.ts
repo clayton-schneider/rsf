7c0382e9a095482105194009b0a18945d4c8d2fe
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-04 09:36:33 -0400

    hopefully we send

--- functions/api/submit.ts
+++ functions/api/submit.ts
@@ -2,6 +2,32 @@ export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
     let pretty = JSON.stringify([...input], null, 2);
+    let send_request = new Request("https://api.mailchannels.net/tx/v1/send", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        personalizations: [
+          {
+            to: [
+              { email: "clayton@simply-sprout.com", name: "Clayton Schneider" },
+            ],
+          },
+        ],
+        from: {
+          email: "noreply@simply-sprout.com",
+          name: "Workers - MailChannels integration",
+        },
+        subject: "Look! No Servers",
+        content: [
+          {
+            type: "text/plain",
+            value: pretty,
+          },
+        ],
+      }),
+    });
     return new Response(pretty, {
       headers: {
         "Content-Type": "application/json;charset=utf-8",
