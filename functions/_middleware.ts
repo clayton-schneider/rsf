03906c4974cdc48b198a52c71a0d1f61f4f8467f
Clayton Schneider <schneiderc2016@gmail.com>
2022-11-04 13:34:52 -0400

    replacing the contents of the functions

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,19 +1,29 @@
-import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
+// import mailChannelsPlugin from "@cloudflare/pages-plugin-mailchannels";
 
-export const onRequest: PagesFunction = mailChannelsPlugin({
-  from: {
-    name: "Website Form Submission",
-    email: "no-reply@simply-sprout.com",
-  },
-  personalizations: [
-    {
-      to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
-    },
-  ],
-  respondWith: () =>
-    new Response(null, {
-      status: 302,
-      headers: { Location: "/enquiries/thank-you" },
-    }),
-  subject: "Form Website Enquiry",
-});
+const errorHandler = async ({ next }) => {
+  try {
+    return await next();
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+  }
+};
+
+export const onRequest = [errorHandler];
+
+// export const onRequest: PagesFunction = mailChannelsPlugin({
+//   from: {
+//     name: "Website Form Submission",
+//     email: "no-reply@simply-sprout.com",
+//   },
+//   personalizations: [
+//     {
+//       to: [{ name: "Clayton Schneider", email: "clayton@simply-sprout.com" }],
+//     },
+//   ],
+//   respondWith: () =>
+//     new Response(null, {
+//       status: 302,
+//       headers: { Location: "/enquiries/thank-you" },
+//     }),
+//   subject: "Form Website Enquiry",
+// });
