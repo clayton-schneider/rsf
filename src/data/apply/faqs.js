df442074d2a0aaf4be4606e56f443d4624be9f23
Clayton Schneider <schneiderc2016@gmail.com>
2023-01-18 11:57:59 -0500

    faqs updated from tony

--- src/data/apply/faqs.js
+++ src/data/apply/faqs.js
@@ -2,21 +2,37 @@ export const faq = [
   {
     question: "When is the application available?",
     answer:
-      "The application is available early at the start of the year until around May. Once we close submissions we take down the application to avoid confusion. It will be posted again at the start of the next year.",
+      "The application is posted on our website sometime in January. Once the deadline for submission is past, the application will be taken down. It will be posted again at the start of the next year.",
   },
   {
-    question: "How do I edit the pdf?",
+    question: "Who is eligible to apply for a scholarship?",
     answer:
-      "There are text areas where you can type out your responses. Simply open the file in your browser and click into the text areas to type.",
+      "Anyone pursuing post secondary education at an accredited institution, and is a Roxbury resident on January 1, is eligible to apply for a scholarship.",
+  },
+  {
+    question: "How can I complete the application?",
+    answer:
+      "The application is in the form of an editable pdf. You can complete it on your computer or laptop",
   },
   {
     question: "How should I submit my material?",
     answer:
-      "You have two options. You can either physically mail all necessary documents to our address. Or, you can scan your supplemental material and email it along with the completed applicaiton.",
+      "You have two options. You can print out the completed application and mail it, along with all necessary supporting documents to our address. The second option is to email your completed application, along with a scan of all the necessary supporting documents, to the roxburyscholarshipfoundatoin@gmail.com.",
   },
   {
     question: "How do I know if I am awarded a scholarship?",
     answer:
       "Be on the lookout for a letter letting you know if you have been awarded a scholarship.",
+  },
+  {
+    question: "If I am awarded a scholarship, how will it be paid?",
+    answer:
+      "Scholarship payments will be sent directly to your school. Your award can only be applied to one school year. You will need to reapply for a scholarship every year that you are in school.",
+  },
+  {
+    question:
+      "If I am awarded a scholarship is there anything else I need to do to receive my award?",
+    answer:
+      "Yes! You will need to send the RSF a statement of charges from your school with the exact address to which payments should be sent; a copy of each award letter if you are receiving scholarships or grants from any sources other than the Roxbury Scholarship Foundation; A copy of your financial package, if you are receiving one from your school and the signed copy of the Statement of Financial Aid that was sent to you with your awards letter. IF WE DO NOT RECEIVE THESE ITEMS, YOUR SCHOLARSHIP WILL NOT BE AWARDED.",
   },
 ];
--- src/data/donate/faqs.js
+++ src/data/donate/faqs.js
@@ -2,22 +2,36 @@ export const faq = [
   {
     question: "How can I make a contribution?",
     answer:
-      "There are two ways to make a contribution: online or through mail. If you click the donate online button, a new screen will open allowing you to donate. Otherwise, you can mail a check addressed to Roxbury Scholarship Foundation to our address: Roxbury Scholarship Foundation, P.O. Box 106 Roxbury, CT 06783.",
+      "There are two ways to make a contribution: online or through the mail. If you click the donate online button, a new screen will open allowing you to donate. Otherwise, you can mail a check addressed to Roxbury Scholarship Foundation to our address: Roxbury Scholarship Foundation, P.O. Box 106 Roxbury, CT 06783.",
+  },
+  {
+    question:
+      "What is the difference between a named memorial scholarship and a general fund scholarship?",
+    answer: `Named memorial scholarships were created to honor a friend or family member. Named scholarships can specify reasonable restrictions relating to the awarding of the scholarship. The law also provides for financial restrictions on fully endowed named scholarships such that their total value cannot go below the originally endowed amount.
+      General fund scholarships are financed from contributions from community members who do not wish to have their donation targeted to a specific named scholarship. There are no restrictions in the awarding of a general fund scholarship. Money in the general fund is also used to finance the day-to-day operations of the RSF.
+      `,
   },
   {
     question:
       "How do I let the foundation know that I want to contribute to a specific memorial scholarship?",
     answer:
-      "If you are donating online there will be an option to enter a note. Please state the memorial scholarhip you would like to donate to there. If you are donating via mail, please send a note with the check specifying the memorial scholarship. If no note is provided then your donation will go to our general fund.",
+      "If you are donating online there will be an option to enter a note. Please state the memorial scholarship you would like to donate to there. If you are donating via mail, please send a note with the check specifying the memorial scholarship. If no note is provided, then your donation will go to our general fund.",
   },
   {
     question:
-      "How do I let the foundation know that I want to establish a new memorial scholarship?",
-    answer: "THIS NEEDS TO BE FILLED IN WITH THE STEPS",
+      "How do I learn more about establishing a named memorial scholarship?",
+    answer:
+      "You can reach out to any of the board members or send us an e-mail at roxburyscholarshipfoundation@gmail.com and we will answer any of your questions.",
+  },
+  {
+    question:
+      "How much money is required to establish a named memorial scholarship?",
+    answer:
+      "If you wish to establish a permanently endowed named scholarship, you will need to raise at least $25,000. However, you have an opportunity to raise this amount over time. A permanently endowed scholarship will never be totally depleted. If you cannot or choose not to reach the $25,000 target, your named scholarship will eventually be totally spent down.",
   },
   {
     question: "How can I become involved?",
     answer:
-      "Email us at roxburyscholarshipfoundation@gmail.com and we will send you the time and place of our next scheduled meeting. We also have opportunities to be a part of the Scholarship Awards Committee that interviews applicants as part of the decision making process.",
+      "Email us at roxburyscholarshipfoundation@gmail.com and we will be happy to discuss opportunities to become either a board member or a member of the Scholarship Awards Committee. You will always be welcome to attend one of the board’s monthly meetings.",
   },
 ];
