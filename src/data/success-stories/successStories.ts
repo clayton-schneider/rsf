305d63aee098ee9097de6b9521b9679d64dd8c4a
Clayton Schneider <schneiderc2016@gmail.com>
2023-02-16 16:57:16 -0500

    emily success stories changes

--- src/data/success-stories/successStories.ts
+++ src/data/success-stories/successStories.ts
@@ -31,9 +31,9 @@ export default [
       "Emily attended Sarah Lawrence College and is now a horticulturist.",
     img: import("@imgs/stories/emily-alworth.jpg"),
     year: "2015",
-    body: `Emily Alworth has the career of her dreams: She is a horticulturist at Red Butte Garden, at the University of Utah. She credits her professors at Sarah Lawrence College for her solid science education, giving her the tools she needed to grow and maintain the newly created zero-scape garden at the University.
+    body: `Emily Alworth has the career of her dreams: She is a horticulturist at Red Butte Garden, at the University of Utah. She credits her professors from both Shepaug High School and Sarah Lawrence College for her solid science education, giving her the tools she needed to grow and maintain the newly created zero-scape garden at the University.
 
-    Emily Alworth has the career of her dreams: She is a horticulturist at Red Butte Garden, at the University of Utah. She credits her professors at Sarah Lawrence College for her solid science education, giving her the tools she needs to grow and maintain the newly created zero-scape garden at the University. She enjoys everything about her work, from designing and installing her new garden, giving classes to students interested in all things botanical, and wrastling the occasional wayward rattlesnake.
+    She enjoys everything about her work, from designing and installing her new garden, giving classes to students interested in all things botanical, and wrastling the occasional wayward rattlesnake.
 
     “The Roxbury Scholarship Fund made it possible for me to receive a fantastic education. Their support was incredible!"`,
     slug: "emily-alworth",
